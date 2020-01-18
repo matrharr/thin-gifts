99dc58f0877be4bd7c60e79e29ce5c07e41b2062
Matt Harris <mattharris@Matts-MBP.frontierlocal.net>
2020-01-17 20:46:42 -0500

    add slider for add-message

--- src/app/add-message/add-message.component.ts
+++ src/app/add-message/add-message.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-message.component.scss']
 })
 export class AddMessageComponent implements OnInit {
+  slides = [
+    'cover',
+    'card',
+    'addressing'
+  ];
+  current = 1;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  moveLeft() {
+    if (this.current > 0) {
+      this.current -= 1;
+    }
+  }
+
+  moveRight() {
+    if (this.current < 2) {
+      this.current += 1;
+    }
+  }
+
+  isCover() {
+    return this.slides[this.current] === 'cover';
+  }
+
+  isCard() {
+    return this.slides[this.current] === 'card';
+  }
+
+  isAddressing() {
+    return this.slides[this.current] === 'addressing';
   }
 
 }
